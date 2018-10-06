904103cad4a8735175d8554a1f541845f37387d0
Tim de Koning <git@king-management.nl>
2018-10-06 22:04:22 +0200

    - Update all depedencies - Update example to typescript

--- example/webpack.example.config.js
+++ example/webpack.example.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    index: path.resolve(__dirname, './index.js'),
+    index: path.resolve(__dirname, './index.tsx'),
   },
   output: {
     path: __dirname,
--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,22 @@ const path = require('path');
 
 module.exports = {
   entry: [
-    './src/index'
+    './src/index',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
   plugins: [],
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   }, /**/
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+      include: path.join(__dirname, 'src'),
+    }],
+  },
 };
