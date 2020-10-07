d889c2d1c74347de4ed1d5e22db0772b9d04b133
Tim de Koning <git@king-management.nl>
2020-10-07 22:35:01 +0200

    Update dependencies (#29)
    More modern eslint
    Prettier
    etc.

--- src/Gauge.tsx
+++ src/Gauge.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 
 //global unique key for every gauge (needed for SVG groups to stay separated)
 let uniqueId = 0;
@@ -16,7 +16,7 @@ export interface GaugeProps {
   topLabelStyle: any;
   valueLabelStyle: any;
   minMaxLabelStyle: any;
-  valueFormatter: (value:number) => string;
+  valueFormatter: (value: number) => string;
 }
 
 export default class Gauge extends React.Component<GaugeProps> {
@@ -28,7 +28,7 @@ export default class Gauge extends React.Component<GaugeProps> {
     width: 400,
     height: 320,
     minMaxLabelsOffset: 25,
-    color: '#fe0400',
+    color: "#fe0400",
     backgroundColor: "#edebeb",
     topLabelStyle: {
       textAnchor: "middle",
@@ -36,10 +36,10 @@ export default class Gauge extends React.Component<GaugeProps> {
       stroke: "none",
       fontStyle: "normal",
       fontVariant: "normal",
-      fontWeight: 'bold',
-      fontStretch: 'normal',
-      lineHeight: 'normal',
-      fillOpacity: 1
+      fontWeight: "bold",
+      fontStretch: "normal",
+      lineHeight: "normal",
+      fillOpacity: 1,
     },
     valueLabelStyle: {
       textAnchor: "middle",
@@ -47,10 +47,10 @@ export default class Gauge extends React.Component<GaugeProps> {
       stroke: "none",
       fontStyle: "normal",
       fontVariant: "normal",
-      fontWeight: 'bold',
-      fontStretch: 'normal',
-      lineHeight: 'normal',
-      fillOpacity: 1
+      fontWeight: "bold",
+      fontStretch: "normal",
+      lineHeight: "normal",
+      fillOpacity: 1,
     },
     minMaxLabelStyle: {
       textAnchor: "middle",
@@ -58,45 +58,49 @@ export default class Gauge extends React.Component<GaugeProps> {
       stroke: "none",
       fontStyle: "normal",
       fontVariant: "normal",
-      fontWeight: 'normal',
-      fontStretch: 'normal',
+      fontWeight: "normal",
+      fontStretch: "normal",
       fontSize: 20,
-      lineHeight: 'normal',
-      fillOpacity: 1
+      lineHeight: "normal",
+      fillOpacity: 1,
     },
-    valueFormatter: (value:number) => `${value}`
+    valueFormatter: (value: number) => `${value}`,
   };
 
   _getPathValues = (value: number) => {
     if (value < this.props.min) value = this.props.min;
     if (value > this.props.max) value = this.props.max;
 
-    let dx = 0;
-    let dy = 0;
+    const dx = 0;
+    const dy = 0;
 
-    let alpha = (1 - (value - this.props.min) / (this.props.max - this.props.min)) * Math.PI;
-    let Ro = this.props.width / 2 - this.props.width / 10;
-    let Ri = Ro - this.props.width / 6.666666666666667;
+    const alpha =
+      (1 - (value - this.props.min) / (this.props.max - this.props.min)) *
+      Math.PI;
+    const Ro = this.props.width / 2 - this.props.width / 10;
+    const Ri = Ro - this.props.width / 6.666666666666667;
 
-    let Cx = this.props.width / 2 + dx;
-    let Cy = this.props.height / 1.25 + dy;
+    const Cx = this.props.width / 2 + dx;
+    const Cy = this.props.height / 1.25 + dy;
 
-    let Xo = this.props.width / 2 + dx + Ro * Math.cos(alpha);
-    let Yo = this.props.height - (this.props.height - Cy) - Ro * Math.sin(alpha);
-    let Xi = this.props.width / 2 + dx + Ri * Math.cos(alpha);
-    let Yi = this.props.height - (this.props.height - Cy) - Ri * Math.sin(alpha);
+    const Xo = this.props.width / 2 + dx + Ro * Math.cos(alpha);
+    const Yo =
+      this.props.height - (this.props.height - Cy) - Ro * Math.sin(alpha);
+    const Xi = this.props.width / 2 + dx + Ri * Math.cos(alpha);
+    const Yi =
+      this.props.height - (this.props.height - Cy) - Ri * Math.sin(alpha);
 
     return { alpha, Ro, Ri, Cx, Cy, Xo, Yo, Xi, Yi };
   };
 
   _getPath = (value: number) => {
-    let { Ro, Ri, Cx, Cy, Xo, Yo, Xi, Yi } = this._getPathValues(value);
+    const { Ro, Ri, Cx, Cy, Xo, Yo, Xi, Yi } = this._getPathValues(value);
 
-    let path = "M" + (Cx - Ri) + "," + Cy + " ";
-    path += "L" + (Cx - Ro) + "," + Cy + " ";
-    path += "A" + Ro + "," + Ro + " 0 0 1 " + Xo + "," + Yo + " ";
-    path += "L" + Xi + "," + Yi + " ";
-    path += "A" + Ri + "," + Ri + " 0 0 0 " + (Cx - Ri) + "," + Cy + " ";
+    let path = `M${Cx - Ri},${Cy} `;
+    path += `L${Cx - Ro},${Cy} `;
+    path += `A${Ro},${Ro} 0 0 1 ${Xo},${Yo} `;
+    path += `L${Xi},${Yi} `;
+    path += `A${Ri},${Ri} 0 0 0 ${Cx - Ri},${Cy} `;
     path += "Z ";
 
     return path;
@@ -105,48 +109,92 @@ export default class Gauge extends React.Component<GaugeProps> {
   private uniqueFilterId: string;
 
   render() {
-    let topLabelStyle = (this.props.topLabelStyle.fontSize
+    const topLabelStyle = this.props.topLabelStyle.fontSize
       ? this.props.topLabelStyle
-      : { ...this.props.topLabelStyle, fontSize: (this.props.width / 10) });
-    let valueLabelStyle = (this.props.valueLabelStyle.fontSize
+      : { ...this.props.topLabelStyle, fontSize: this.props.width / 10 };
+    const valueLabelStyle = this.props.valueLabelStyle.fontSize
       ? this.props.valueLabelStyle
-      : { ...this.props.valueLabelStyle, fontSize: (this.props.width / 5) });
-      let {minMaxLabelsOffset} = this.props;
-    let { Cx, Ro, Ri, Xo, Cy, Xi } = this._getPathValues(this.props.max);
-    if (!this.uniqueFilterId) this.uniqueFilterId = "filter_" + uniqueId++;
+      : { ...this.props.valueLabelStyle, fontSize: this.props.width / 5 };
+    const { minMaxLabelsOffset } = this.props;
+    const { Cx, Ro, Ri, Xo, Cy, Xi } = this._getPathValues(this.props.max);
+    if (!this.uniqueFilterId) this.uniqueFilterId = `filter_${uniqueId++}`;
     return (
-      <svg height="100%" version="1.1" width="100%" xmlns="http://www.w3.org/2000/svg" style={{
-        width: this.props.width,
-        height: this.props.height,
-        overflow: 'hidden',
-        position: 'relative',
-        left: 0,
-        top: 0
-      }}>
+      <svg
+        height="100%"
+        version="1.1"
+        width="100%"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{
+          width: this.props.width,
+          height: this.props.height,
+          overflow: "hidden",
+          position: "relative",
+          left: 0,
+          top: 0,
+        }}
+      >
         <defs>
           <filter id={this.uniqueFilterId}>
-            <feOffset dx="0" dy="3"/>
-            <feGaussianBlur result="offset-blur" stdDeviation="5"/>
-            <feComposite operator="out" in="SourceGraphic" in2="offset-blur" result="inverse"/>
-            <feFlood floodColor="black" floodOpacity="0.2" result="color"/>
-            <feComposite operator="in" in="color" in2="inverse" result="shadow"/>
-            <feComposite operator="over" in="shadow" in2="SourceGraphic"/>
+            <feOffset dx="0" dy="3" />
+            <feGaussianBlur result="offset-blur" stdDeviation="5" />
+            <feComposite
+              operator="out"
+              in="SourceGraphic"
+              in2="offset-blur"
+              result="inverse"
+            />
+            <feFlood floodColor="black" floodOpacity="0.2" result="color" />
+            <feComposite
+              operator="in"
+              in="color"
+              in2="inverse"
+              result="shadow"
+            />
+            <feComposite operator="over" in="shadow" in2="SourceGraphic" />
           </filter>
         </defs>
-        <path fill={this.props.backgroundColor} stroke="none" d={this._getPath(this.props.max)}
-              filter={"url(#" + this.uniqueFilterId + ")"}/>
-        <path fill={this.props.color} stroke="none" d={this._getPath(this.props.value)}
-              filter={"url(#" + this.uniqueFilterId + ")"}/>
-        <text x={this.props.width / 2} y={this.props.height / 8} textAnchor="middle" style={topLabelStyle}>
+        <path
+          fill={this.props.backgroundColor}
+          stroke="none"
+          d={this._getPath(this.props.max)}
+          filter={`url(#${this.uniqueFilterId})`}
+        />
+        <path
+          fill={this.props.color}
+          stroke="none"
+          d={this._getPath(this.props.value)}
+          filter={`url(#${this.uniqueFilterId})`}
+        />
+        <text
+          x={this.props.width / 2}
+          y={this.props.height / 8}
+          textAnchor="middle"
+          style={topLabelStyle}
+        >
           {this.props.label}
         </text>
-        <text x={this.props.width / 2} y={this.props.height / 5 * 4} textAnchor="middle" style={valueLabelStyle}>
-          { this.props.valueFormatter(this.props.value) }
+        <text
+          x={this.props.width / 2}
+          y={(this.props.height / 5) * 4}
+          textAnchor="middle"
+          style={valueLabelStyle}
+        >
+          {this.props.valueFormatter(this.props.value)}
         </text>
-        <text x={((Cx - Ro) + (Cx - Ri)) / 2} y={Cy + minMaxLabelsOffset} textAnchor="middle" style={this.props.minMaxLabelStyle}>
+        <text
+          x={(Cx - Ro + (Cx - Ri)) / 2}
+          y={Cy + minMaxLabelsOffset}
+          textAnchor="middle"
+          style={this.props.minMaxLabelStyle}
+        >
           {this.props.min}
         </text>
-        <text x={(Xo + Xi) / 2} y={Cy + minMaxLabelsOffset} textAnchor="middle" style={this.props.minMaxLabelStyle}>
+        <text
+          x={(Xo + Xi) / 2}
+          y={Cy + minMaxLabelsOffset}
+          textAnchor="middle"
+          style={this.props.minMaxLabelStyle}
+        >
           {this.props.max}
         </text>
       </svg>
